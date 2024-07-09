ec5c798d38365d4f49c1fa39ae9235a70d40feb9
divesh0001 <sainidivesh259@gmail.com>
2024-07-09 17:39:24 +0530

    updated dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Stocks-God"
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    open: true,
+  },
+  base: './',
+
+});
